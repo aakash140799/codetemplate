87525a993ea51d6942e97b2c7fe9979b23265db9
aakash140799 <34199858+aakash140799@users.noreply.github.com>
2019-08-15 04:36:36 -0700

    Update simplex.cpp

--- simplex.cpp
+++ simplex.cpp
@@ -1,9 +1,18 @@
 
 
-// simplex algorithm
+
 const int arrsize = 100;
-struct linearprogram
+// linear-program defined by
+// standard form
+// Ax <= b;
+// maximize cx
+//
+// slack formm
+// basic[] = b - Ax
+// maximize v + c * nonbasic[]
+class linearprogram
 {
+	public:
 	float A[arrsize][arrsize];
 	float b[arrsize];
 	float c[arrsize];
@@ -19,12 +28,16 @@ linearprogram Lt;
 float del[arrsize];
 float ct[arrsize];
 
+// convert standard form to slack form
 void standardtoslack(linearprogram &L)
 {
+	// set basic and non-basic variable count
 	Lt.n = L.n;
 	Lt.m = L.m;
 	int n = L.n;
 	int m = L.m;
+	
+	// place cofficients according to index in basic variables
 	for(int i = 0;i < m;i++)
 	{
 		for(int j = 0;j < n;j++)
@@ -40,6 +53,8 @@ void standardtoslack(linearprogram &L)
 	{
 		Lt.c[i] = L.c[i];
 	}
+	
+	// place basic and non-basic varibles index
 	for(int i = 0;i < n;i++)
 	{
 		Lt.nonbasic[i]  = i;
@@ -50,12 +65,15 @@ void standardtoslack(linearprogram &L)
 	}
 	Lt.v = 0;
 
+	// copy new linearprogram into orginial
 	L = Lt;
 }
 
 
+// swap a non-basic variable e, by basic variable l
 void pivot(linearprogram &L, int l,int e)
 {
+	// change linear equation pointed by basic variable l
 	Lt.b[e] = L.b[l]/L.A[l][e];
 	for(int l1 = 0; l1 < L.n; l1++)
 	{
@@ -65,6 +83,7 @@ void pivot(linearprogram &L, int l,int e)
 	}Lt.A[e][l] = 1/L.A[l][e];
 
 
+	// change linear equation pointed by other basic variables
 	for(int l1 = 0; l1 < L.m; l1++)
 	{
 		int i = L.basic[l1];
@@ -77,14 +96,17 @@ void pivot(linearprogram &L, int l,int e)
 		}Lt.A[i][l] = -(L.A[i][e]*Lt.A[e][l]);
 	}
 
+	// change objective function
 	for(int l1 = 0; l1 < L.n; l1++)
 	{
 		int j = L.nonbasic[l1];
 		Lt.c[j] = L.c[j] - (L.c[e]*Lt.A[e][j]);
 	}Lt.c[l] = -(L.c[e]*L.A[e][l]);
 
+	// change objective function constant
 	Lt.v = L.v + (L.c[e]*Lt.b[e]);
 	
+	// swap l, e in basic, and non-basic array
 	int li, ei;
 	for(int l1 = 0; l1 < L.n; l1++)
 	{
@@ -98,63 +120,79 @@ void pivot(linearprogram &L, int l,int e)
 	}
 	swap(Lt.basic[li], Lt.nonbasic[ei]);
 
+	// copy new linear-program into original
 	L = Lt;
 }
 
+// pivot in linearprogram untill, all coficients in objective function are negative
 bool basicloop(linearprogram &L)
 {
 	while(true)
 	{
+		// search for non-negative cofficient non-basic variable in objective function
 		int e = -1;
 		for(int l1 = 0; l1 < L.n; l1++)
 		{
 			int j = L.nonbasic[l1];
 			if(L.c[j] > 0){e = j;break;}
 		}
+		// if none, exit
 		if(e == -1){break;}
-
+		
+		// search for basic variable, which is most constraining
 		int l = -1;
 		for(int l1 = 0; l1 < L.m; l1++)
 		{
 			int i = L.basic[l1];
 			if(L.A[i][e] > 0 && (l == -1 || L.b[i]/L.A[i][e] < L.b[l]/L.A[l][e])){l = i;}
 		}
-
+		// if not constraint, return unbounded
 		if(l == -1){cout << "unbounded\n"; return false;}
-
+		
+		// swap a non-basic variable with non-negative objective function cofficient
 		pivot(L, l, e);
 	}
 	return true;
 }
 
+
+// convert original linear program into equivalent linear program with initial feasible solution
 bool initialisePreflow(linearprogram &L)
 {
+	// check for initial feasible solution
 	int minb = L.basic[0];
 	for(int l1 = 0; l1 < L.m; l1++)
 	{
 		int j = L.basic[l1];
 		if(L.b[j] < L.b[minb]){minb = j;}
 	}
-
+	// if initial solution (all non-basic zeroes) feasible
 	if(L.b[minb] >= 0)		// all b's are positive
 	{
+		// return;
 		return true;
 	}
+	// change to feasible
 	else
 	{
+		// store objective function
 		int n = L.n;
 		int m = L.m;
 		for(int l1 = 0; l1 < L.n+L.m; l1++)
 		{
 			ct[l1] = L.c[l1];
 		}
-											// add new non-basic index n+m;
+		
+		
+		// add new non-basic index n+m;
 		for(int l1 = 0; l1 < L.m; l1++)
 		{
 			int i = L.basic[l1];
 			L.A[i][n+m] = -1;
 		}
 
+		
+		// change objective function
 		for(int l1 = 0; l1 < L.n+L.m; l1++)
 		{
 			L.c[l1] = 0;
@@ -167,30 +205,32 @@ bool initialisePreflow(linearprogram &L)
 		int l = minb;
 		int e = n+m;
 		pivot(L, l, e);
-
+		
+		// pivot untill initial solution is feasible
 		basicloop(L);
 
+		// check for initial solution
 		for(int l1 = 0; l1 < L.n; l1++)
 		{
 			int j = L.nonbasic[l1];
 			del[j] = 0;
 		}
-
 		for(int l1 = 0; l1 < L.m; l1++)
 		{
 			int i = L.basic[l1];
 			del[i] = L.b[i];
 		}
-
+		
+		// if not feasible, return
 		if(del[n+m] != 0){cout << "infeasible\n";return false;}
-
+		
+		// delete varible n+m
 		l = -1;
 		for(int l1 = 0; l1 < L.m; l1++)
 		{
 			int i = L.basic[l1];
 			if(i == n+m){l = l1;break;}
 		}
-
 		if(l != -1)
 		{
 			int e = -1;
@@ -201,12 +241,12 @@ bool initialisePreflow(linearprogram &L)
 			}
 			if(e != -1){pivot(L, l, e);}
 		}
-
 		for(int l1 = 0; l1 < L.n; l1++)
 		{
 			if(L.nonbasic[l1] == n+m){l = l1;break;}
 		}
 
+		// restore objective function
 		for(int l1 = 0; l1 < L.n+L.m; l1++)
 		{
 			L.c[l1] = 0;
@@ -225,7 +265,6 @@ bool initialisePreflow(linearprogram &L)
 				L.c[j] += ct[i]*L.A[i][j];
 			}
 		}
-
 		swap(L.nonbasic[l], L.nonbasic[L.n-1]);
 		L.n--;
 
@@ -233,13 +272,16 @@ bool initialisePreflow(linearprogram &L)
 	}
 }
 
-
+// simplex algorithm for solving linear-programming problems
 void simplex()
 {
+	// change from standard to slack form;
 	standardtoslack(L);
+	// check for initial feasible solution
 	bool feasible = initialisePreflow(L);
 	if(feasible)
 	{
+		// pivot ,untill objective function has all neagtive cofficients
 		basicloop(L);
 	}
 }
