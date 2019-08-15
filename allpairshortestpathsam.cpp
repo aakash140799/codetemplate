7c3ad4033f29a631485bd143dbdd32b484fa9fca
aakash140799 <34199858+aakash140799@users.noreply.github.com>
2019-08-14 19:21:13 -0700

    Update allpairshortestpathsam.cpp

--- allpairshortestpathsam.cpp
+++ allpairshortestpathsam.cpp
@@ -5,7 +5,7 @@ const int vsize = 100;
 const int64_t posinf = 1000000000;
 
 // edge relaxation using matrix multiplication
-void extendedshortestpath(int64_t a[vsize][vsize],int64_t b[vsize][vsize],int64_t c[vsize][vsize],int n)
+void extendedshortestpath(int64_t a[vsize][vsize],  int64_t b[vsize][vsize],  int64_t c[vsize][vsize],  int n)
 {
 	for(int i = 0;i < n;i++)
 	{
@@ -23,10 +23,12 @@ void extendedshortestpath(int64_t a[vsize][vsize],int64_t b[vsize][vsize],int64_
 
 
 int64_t l[vsize][vsize], w[vsize][vsize], t[vsize][vsize];
+// w[u][v] - weight of edge[u][v] if exists, else posinf
+// l[u][v] - shortest path distance between (u,v) if exists, else posinf
 // shortest path using matrix multiplication and , square and multiplication
 void allpairshortestpathsam(int n)
 {
-	// initializes l to w , l = w(0);
+	// initializes l to w , l = w(1);
 	for(int i = 0;i < n;i++)
 	{
 		for(int j = 0;j < n;j++)
@@ -39,7 +41,7 @@ void allpairshortestpathsam(int n)
 	int p = 1;
 	while(p < n)
 	{
-		extendedshortestpath(l, l, t, n);	// sets t = w(p)*w(p);
+		extendedshortestpath(l, l, t, n);	// sets t = l*l, and l = w(p)
 		for(int i = 0;i < n;i++)
 		{
 			for(int j = 0;j < n;j++)
