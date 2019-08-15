9742a0f677dd7e5ad1e633c26fe8454b4d90b2ed
aakash140799 <34199858+aakash140799@users.noreply.github.com>
2019-08-15 04:42:26 -0700

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -54,13 +54,14 @@ template<class type> void quicksort(type *arr,int p,int r)
 		type hing = arr[r-1];
 		int il = p;
 		int ih = r-1;
-
+		
+		// pivot around hing
 		for(int i = p;i < ih;i++)
 		{
 			if(arr[i] < hing){swap(arr[i],arr[il]);il++;}
 			else if(arr[i] == hing){swap(arr[i],arr[ih-1]);ih--;i--;}
 		}
-
+		// relocate hing value in sorted order
 		for(int i = 0;i < r-ih;i++)
 		{
 			swap(arr[il+i], arr[ih+i]);
@@ -74,28 +75,37 @@ template<class type> void quicksort(type *arr,int p,int r)
 
 
 int countarr[radsize];
-int radix(arrtyp,int);
-int radlen(arrtyp);
+
+// functions to be maually defined accordinly of type;
+int radix(type,int);
+int radlen(type);
 
 
-template<class type> void countsort(type *arr,int p,int r,int k)
+template<class type> void countsort(type *arr,int p,int r,int k=0)
 {
+	// set to zero
 	for(int i = 0;i < radsize;i++){countarr[i] = 0;}
+	// find count of all radix values
 	for(int i = p;i < r;i++){countarr[radix(arr[i], k)]++;}
 
+	// find end-index of all values
 	for(int i = 1;i < radsize;i++){countarr[i] += countarr[i-1];}
+	// place all values accordingly of end-index
 	for(int i = r-1;i >= p;i--){tarr[countarr[radix(arr[i], k)]-1] = arr[i];countarr[radix(arr[i], k)]--;}
+	// copy sorted array into original array
 	for(int i = 0;i < r-p;i++){arr[i+p] = tarr[i];}
 }
 
 template<class type> void radixsort(type *arr,int p,int r)
 {
 	int k = 0;
+	// find max rad length
 	for(int i = p;i < r;i++)
 	{
 		if(k < radlen(arr[i])){k = radlen(arr[i]);}
 	}
-
+	
+	// sort according to radix index
 	for(int l = 0;l < k;l++)
 	{
 		countsort<type>(arr, p, r, l);
